cd85d92ebb539bf8e3be40f6cf0336cd6d0a51b9
Bulat R. Tukbaev <tukbaevbr@yandex.ru>
2022-09-12 09:50:36 +0500

    Add frame

---
+++ src/Commands/TestCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TestCommand extends Command
+{
+    protected static $defaultName = 'test';
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return Command::SUCCESS;
+    }
+}
